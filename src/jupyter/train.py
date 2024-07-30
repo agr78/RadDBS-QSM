89684f107c89b269c10a077b58a242bfe71d948f
agr78 <agr78@cornell.edu>
2024-07-30 17:30:22 -0400

    Added `rs` variable to `train.py`

--- src/jupyter/train.py
+++ src/jupyter/train.py
@@ -70,6 +70,12 @@ def train_estimator(subsc,X_all_c,K_all_c,per_change,pre_updrs_off,aug,reg,save,
         X0_ss0,_,X_test_ss0 = util.model_scale(skp.StandardScaler(),
                                                     X_train,train_index,X_test,
                                                     test_index,pre_updrs_off,None,None,None,None,None,None,None,None,None,False,False,False)
+        if aug == 'smogn':
+            X_smogn,y_smogn,idx_kept,sscaler = util.rad_smogn(X0_ss0,y_train,np.amax(y_train),np.amin(y_train),1,0,0.05,0.02,rs0)
+            X0_ss0 = np.vstack((X0_ss0,X_smogn))
+            y_train = np.hstack((y_train,y_smogn))
+            y_cat = y_train <= 0.3
+
         cvn = 5
         cv_scores = np.zeros((cvn,1))
         rcfs = 1000
@@ -103,10 +109,7 @@ def train_estimator(subsc,X_all_c,K_all_c,per_change,pre_updrs_off,aug,reg,save,
             y_train = np.hstack((y_train,y_train_n))
             y_cat = y_train <= 0.3
             X0_ss0 = np.vstack((X0_ss0,X0_ss0))
-          if aug == 'smogn':
-            X_smogn,y_smogn,idx_kept,sscaler = util.rad_smogn(X0_ss0,y_train,np.amax(y_train),np.amin(y_train),1,0,0.05,0.02,rs)
-            X0_ss0 = np.vstack((X0_ss0,X_smogn))
-            y_train = np.hstack((y_train,y_smogn))
+   
             
         for jj in np.arange(2,cvn):
           skf_g = sms.StratifiedKFold(n_splits=jj,shuffle=True,random_state=0)
